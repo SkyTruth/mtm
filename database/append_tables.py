fb51fa17fbb059e07b0934b6f3d9b1bb8f2ee02c
Christian Thomas <christian@skytruth.org>
2025-09-23 14:40:29 -0400

    updated commenting

--- database/append_tables.py
+++ database/append_tables.py
@@ -230,8 +230,8 @@ def append_to_permits_table_from_local():
 
 
 if __name__ == "__main__":
-    # append_to_annual_mining_table_from_local()
-    # append_to_annual_mining_table_from_gcs()
-    # append_to_highwall_centerline_table_from_local()
-    # append_to_counties_table_from_local()
+    append_to_annual_mining_table_from_local()
+    append_to_annual_mining_table_from_gcs()
+    append_to_highwall_centerline_table_from_local()
+    append_to_counties_table_from_local()
     append_to_permits_table_from_local()
