53f7c832c270d51b8b0ed8fc11175d1de5a80dad
Christian Thomas <christian@skytruth.org>
2025-10-20 16:16:17 -0400

    added functionality to create HUC table

--- database/make_tables.py
+++ database/make_tables.py
@@ -177,9 +177,53 @@ def create_permits_table():
         print(f"Successfully created table: {table_name}.")
 
 
+def create_huc_table():
+    engine = connect_tcp_socket()
+    table_name = "huc_boundaries"
+
+    # SQL statement for creating table
+    create_stmt = f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            objectid                    INT,
+            tmnid                       TEXT,
+            metasourceid                TEXT,
+            sourcedatadesc              TEXT,
+            sourceoriginator            TEXT,
+            sourcefeatureid             TEXT,
+            loaddate                    DATE,
+            referencegnis_ids           TEXT,
+            areaacres                   DOUBLE PRECISION,
+            areasqkm                    DOUBLE PRECISION,
+            states                      TEXT,
+            huc10                       TEXT,
+            name                        TEXT,
+            hutype                      TEXT,
+            humod                       TEXT,
+            globalid                    TEXT,
+            shape_Length                DOUBLE PRECISION,
+            shape_Area                  DOUBLE PRECISION,
+            hutype_description          TEXT,
+            huc12                       TEXT,
+            tohuc                       TEXT,
+            noncontributingareaacres    DOUBLE PRECISION,
+            noncontributingareasqkm     DOUBLE PRECISION,
+            huc2                        TEXT,
+            huc4                        TEXT,
+            huc6                        TEXT,
+            huc8                        TEXT,
+            geom                        geometry(MultiPolygon, 4326)
+        );
+    """
+
+    with engine.connect() as conn:
+        conn.execute(sqlalchemy.text(create_stmt))
+        conn.commit()
+        print(f"Successfully created table: {table_name}.")
+
 if __name__ == "__main__":
     create_annual_mining_table()
     create_cumulative_mining_table()
     create_highwall_centerlines_table()
     create_counties_table()
     create_permits_table()
+    create_huc_table()
